97ac93b2a590a4f586f9021ba58bdf34d0de7b0b
Guido Van Hoof <guido.van.hoof3@telenet.be>
2015-12-03 19:56:12 +0100

    refactor ls to class Ls

--- chapter5/ls.py
+++ chapter5/ls.py
@@ -1,59 +1,69 @@
 import argparse
+import os
+import os.path
 
 
-def create_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-H", "--hidden",
-        action="store_true",
-        help="show hidden files [default: off]",
-        default=False
-    )
-    parser.add_argument(
-        "-m", "--modified",
-        action="store_true",
-        help="show hidden last modified date/time [default: off]",
-        default=False
-    )
-    parser.add_argument(
-        "-o", "--order",
-        help="order by ('name', 'n', 'modified', 'm', 'size', 's') [default: name]",
-        default="name",
-        choices=['name', 'n', 'modified', 'm', 'size', 's']
-    )
-    parser.add_argument(
-        "-r", "--recursive",
-        action="store_true",
-        help="recurse into subdirectories [default: off]",
-        default=False
-    )
-    parser.add_argument(
-        "-s", "--sizes",
-        action="store_true",
-        help="show sizes [default: off]",
-        default=False
-    )
-    parser.add_argument(
-        "dirs",
-        nargs="*",
-        help="The dirs are optional. If not given, '.' is used.",
-        default=list(".")
-    )
-    return parser
+class Ls(object):
 
+    def __init__(self, path_walker=os.walk, size_reader=os.path.getsize):
+        self.path_walker = path_walker
+        self.size_reader = size_reader
+        self.parser = self._create_parser()
+        self.args = self.parser.parse_args()
 
-def remove_hidden(names):
-    return [name for name in names if not name.startswith(".")]
+    def _create_parser(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "-H", "--hidden",
+            action="store_true",
+            help="show hidden files [default: off]",
+            default=False
+        )
+        parser.add_argument(
+            "-m", "--modified",
+            action="store_true",
+            help="show hidden last modified date/time [default: off]",
+            default=False
+        )
+        parser.add_argument(
+            "-o", "--order",
+            help="order by ('name', 'n', 'modified', 'm', 'size', 's') [default: name]",
+            default="name",
+            choices=['name', 'n', 'modified', 'm', 'size', 's']
+        )
+        parser.add_argument(
+            "-r", "--recursive",
+            action="store_true",
+            help="recurse into subdirectories [default: off]",
+            default=False
+        )
+        parser.add_argument(
+            "-s", "--sizes",
+            action="store_true",
+            help="show sizes [default: off]",
+            default=False
+        )
+        parser.add_argument(
+            "dirs",
+            nargs="*",
+            help="The dirs are optional. If not given, '.' is used.",
+            default=list(".")
+        )
+        return parser
 
+    def _get_file_size(self, file_name):
+        return self.size_reader(file_name)
 
-def show_info(filename, args):
-    return ""
+    def _remove_hidden(self, names):
+        return [name for name in names if not name.startswith(".")]
+
+    def show_info(self, filename, args):
+        return ""
     
     
 def main():
-    parser = create_parser()
-    args = parser.parse_args()
-    print("args = ", args)
+    ls = Ls()
+    print("args = ", ls.args)
 
 
 if __name__ == "__main__":
--- chapter5/test_ls.py
+++ chapter5/test_ls.py
@@ -5,69 +5,75 @@ import ls
 
 SIZE = 1365
 
-def get_size(filenam):
+
+def os_path_getsize(filename):
     return SIZE
+
+
+def os_walk(directory):
+    return None
 
 
 class TestLs(unittest.TestCase):
 
     def setUp(self):
-        self.parser = ls.create_parser()
+        self.ls = ls.Ls(path_walker=os_walk, size_reader=os_path_getsize)
 
     def tearDown(self):
         pass
 
     def test_optie_hidden_met_default_waarde_false_aanwezig(self):
         """Optie 'hidden' met default waarde 'False'"""
-        args = self.parser.parse_args()
-        self.assertFalse(args.hidden, "Optie 'hidden' niet aanwezig !")
+        self.assertFalse(self.ls.args.hidden, "Optie 'hidden' niet aanwezig !")
 
     def test_optie_modified_met_default_waarde_false_aanwezig(self):
         """Optie 'modified' met default waarde 'False'"""
-        args = self.parser.parse_args()
-        self.assertFalse(args.modified, "Optie 'modified' niet aanwezig !")
+        self.assertFalse(self.ls.args.modified, "Optie 'modified' niet aanwezig !")
 
     def test_optie_order_met_default_waarde_name_aanwezig(self):
         """Optie 'order' met default waarde 'name'"""
-        args = self.parser.parse_args()
-        self.assertEqual(args.order, "name", "Optie 'order' niet aanwezig !")
+        self.assertEqual(self.ls.args.order, "name", "Optie 'order' niet aanwezig !")
 
     def test_optie_recursive_met_default_waarde_false_aanwezig(self):
         """Optie 'recursive' met default waarde 'False'"""
-        args = self.parser.parse_args()
-        self.assertFalse(args.recursive, "Optie 'recursive' niet aanwezig !")
+        self.assertFalse(self.ls.args.recursive, "Optie 'recursive' niet aanwezig !")
 
     def test_optie_sizes_met_default_waarde_false_aanwezig(self):
         """Optie 'sizes' met default waarde 'False'"""
-        args = self.parser.parse_args()
-        self.assertFalse(args.sizes, "Optie 'sizes' niet aanwezig !")
+        self.assertFalse(self.ls.args.sizes, "Optie 'sizes' niet aanwezig !")
 
     def test_optie_dirs_met_default_waarde_dot_aanwezig(self):
         """Optie 'dirs' met default waarde '.'"""
-        args = self.parser.parse_args()
-        self.assertIn(".", args.dirs, "Optie 'dirs' niet aanwezig !")
+        self.assertIn(".", self.ls.args.dirs, "Optie 'dirs' niet aanwezig !")
 
     def test_verwijdert_verborgen_bestanden(self):
         """Functie 'remove_hidden' verwijdert verborgen bestanden/directories"""
         expected_dirnames = []
         dirnames = [".hidden_directory"]
-        dirnames = ls.remove_hidden(dirnames)
-        self.assertEqual(expected_dirnames, dirnames, "Verborgen bestanden niet verwijderd !")
+        actual_dirnames = self.ls._remove_hidden(dirnames)
+        self.assertEqual(expected_dirnames, actual_dirnames, "Verborgen bestanden niet verwijderd !")
 
     def test_verwijdert_geen_zichtbare_bestanden(self):
         """Functie 'remove_hidden' verwijdert geen zichtbare bestanden/directories"""
         expected_dirnames = ["zichtbaar"]
         dirnames = [".hidden_directory", "zichtbaar"]
-        dirnames = ls.remove_hidden(dirnames)
-        self.assertEqual(expected_dirnames, dirnames, "Verborgen bestanden niet verwijderd !")
+        actual_dirnames = self.ls._remove_hidden(dirnames)
+        self.assertEqual(expected_dirnames, actual_dirnames, "Verborgen bestanden niet verwijderd !")
 
-    def test_show_info_toont_size_bij_sizes_true(self):
-        os.path.getsize = get_size
-        filename = "./ls.py"
-        args = self.parser.parse_args()
-        args.size = True
-        output = ls.show_info(filename, args)
-        self.assertIn(str(SIZE), output, "Size wordt niet getoond !")
+    def test_grootte_bestand_wordt_opgehaald(self):
+        """Functie _get_file_size geeft bestandsgrootte terug"""
+        file_name = "./ls.py"
+        expected_size = SIZE
+        actual_size = self.ls._get_file_size(file_name)
+        self.assertEqual(expected_size, actual_size, "Bestandsgrootte niet correct !")
+
+    # def test_show_info_toont_size_bij_sizes_true(self):
+    #     # os.path.getsize = get_size
+    #     filename = "./ls.py"
+    #     # self.ls.args = self.parser.parse_self.ls.args()
+    #     self.ls.args.size = True
+    #     output = self.ls.show_info(filename, self.ls.args)
+    #     self.assertIn(str(SIZE), output, "Size wordt niet getoond !")
         
 
 if __name__ == "__main__":
